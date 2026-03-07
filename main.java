3ba4cda93432257cdd56106d0a39ba00f70e430a
Gazach <gamerzcraft37@gmail.com>
2026-03-07 20:11:55 +0700

    tes lagi lagi

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello,Gaza lazuardi");
+        System.out.println("Hello,niko las las las");
     }
 }
