2b72c16fe4504b57a28fe02592170662370b6484
Gazach <gamerzcraft37@gmail.com>
2026-03-08 15:48:44 +0700

    ngebuat loop buat game dan nambah kotak biru

--- main.java
+++ main.java
@@ -1,6 +1,26 @@
+import javax.swing.JFrame;
+import system.GamePanel;
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello,Gaza lazuardi");
-        System.out.println("Hello,niko las las las");
+        
+        //Membuat window atau frame untuk game
+        JFrame window = new JFrame();
+        // =konfigurasi window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Agar ketika di close programnya juga berhenti
+        window.setResizable(true); //Agar ukuran window bisa diubah-ubah
+        window.setTitle("Gani's Town"); //Judul game
+
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+        //Ngejalanin game loop atau game thread
+        gamePanel.startGameThread();
     }
 }
---
+++ system/GamePanel.java
@@ -0,0 +1,78 @@
+package system;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class GamePanel extends JPanel implements Runnable{
+
+	//LAYAR AWAL
+	//Untuk per tile 
+	final int originalTileSize = 16; //JADI INI 16X16
+	final int scale = 3;//INI SCALE NYA JADI 16X3 ITU KAN 48 JADI 48 PIXEL 
+	final int tileSize = originalTileSize * scale; //Perhitungan untuk yang atas
+	//Untuk besar layarnya
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int besarLayar = tileSize * maxScreenCol; //Jadi besarnya 768
+	final int tinggiLayar = tileSize * maxScreenRow; //Tingginya 576
+
+	Thread gameThread;
+
+	public GamePanel(){
+
+		this.setPreferredSize(new Dimension(besarLayar, tinggiLayar));
+		this.setBackground(Color.white);
+		this.setDoubleBuffered(true);
+	}
+	public void startGameThread(){
+
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	//Game Loop
+	@Override
+	public void run() {
+		// mengatur untuk update dan render game setiap 60 FPS
+		double drawInterval = 1000000000 / 60; // 60 FPS
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+
+		while(gameThread != null){
+
+			currentTime = System.nanoTime();
+
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+
+			if(delta >= 1){
+
+				update();
+				repaint();
+
+				delta--;
+			}
+		}
+	}
+
+	public void update() {
+		// Update logika game di sini
+	}
+
+	@Override
+	public void paintComponent(java.awt.Graphics g) {
+		super.paintComponent(g);
+		// Render grafis game di sini
+		Graphics2D g2 = (Graphics2D) g;
+		// Contoh menggambar kotak biru di layar
+		g2.setColor(Color.blue);
+
+		g2.fillRect(0, 0, 100, 100);
+
+		g2.dispose();
+	}
+}
