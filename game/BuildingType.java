d3e33d7bcbec1014f90f32e53494e344d5fc5d45
niko <mnikolas036@gmail.com>
2026-05-06 17:12:50 +0700

    Dibenerinnya animasi

--- game/BuildingType.java
+++ game/BuildingType.java
@@ -12,11 +12,11 @@ public enum BuildingType {
     // Kategori:
     //   HOUSING  → bangunan tempat tinggal, memberikan populasi (minPeople–maxPeople) per unit, incomePerSecond = 0
     //   PRODUCTION → bangunan produksi, menghasilkan uang per detik, minPeople/maxPeople = 0
-    //
-    //                          w  h    price   category                minP maxP maxW income/s  animFrameCount  animSpeed  assetName
+
+    //                  w          h        price        category                    minP            maxP         maxW income/s  animFrameCount  animSpeed  assetName
     HOUSE(          1, 1,   500, BuildingCategory.HOUSING,    2,   4,  0,  0,  0,  1,  "building_1x1"),
-    WHEAT(          2, 2,   800, BuildingCategory.HOUSING,    4,   8,  0,  0,  0,  1,  "wheat"),
-    BUILDING_2X2(   2, 2,  1500, BuildingCategory.PRODUCTION, 0,   0,  4, 10,  5,  12, "building_2x2"),  // 60/6 = 10 fps
+    WHEAT(          2, 2,   800, BuildingCategory.HOUSING,    0,   8,  4,  10,  8,  12,  "wheat"),
+    BUILDING_2X2(   2, 2,  1500, BuildingCategory.PRODUCTION, 0,   0,  4, 10,  11,  12, "building_2x2"),  // 60/6 = 10 fps
     WINDMILL(       2, 3,  3000, BuildingCategory.PRODUCTION, 0,   0,  6, 25,  3,  6,  "windmill"),       // 60/6 = 10 fps
     BARN(           2, 4,  2000, BuildingCategory.PRODUCTION, 0,   0,  4, 10,  5,  12, "barn");
     // -------------------------------------------------------------------------
